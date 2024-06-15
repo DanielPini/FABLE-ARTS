5950cc3368b78c00737d28f808847f2f032c91d1
DanielPini <danielpini7@hotmail.com>
2024-06-15 12:42:04 +0200

    Modules. May undo later...

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
-import { scrollButtonJS } from "./scroll_button";
-import { playStems } from "./stems";
-import { bambooAnimation } from "./bamboo_animation";
+import playStems from "./stems";
 
-scrollButtonJS();
+import scrollButtonJS from "./scroll_button";
+
+import bambooAnimation from "./bamboo_animation";
+
 playStems();
+scrollButtonJS();
 bambooAnimation();
