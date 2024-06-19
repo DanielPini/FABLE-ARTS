35ec4c11c626519cc49636acad2b1f47ba5376af
DanielPini <danielpini7@hotmail.com>
2024-06-19 16:27:59 +0200

    Fixed bamboo animation!!!

--- js/bamboo_animation.js
+++ js/bamboo_animation.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
   const observerOptions = {
-    root: null, // Use the viewport as the root
+    // root: null, // Use the viewport as the root
     rootMargin: '0px',
     threshold: 0 // Trigger when the element is visible
   };
