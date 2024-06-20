ebeb1944f5798bbd37e64f0f1d5c9d37d8ea45ff
DanielPini <danielpini7@hotmail.com>
2024-06-20 12:47:03 +0200

    Added content to About page. CSS not linking...

--- js/splash.js
+++ js/splash.js
@@ -1,5 +1,15 @@
 const splashScreen = document.querySelector('.splash');
-splashScreen.addEventListener('click', () => {splashScreen.style.opacity = 0;
-  setTimeout(() => {
-    splashScreen.classList.add('splash-hidden')}, 610);
-})
+let fired = sessionStorage.getItem("splashTriggered");
+
+if (!fired) {
+  splashScreen.style.opacity = 1;
+  splashScreen.style.zIndex = 1;
+  splashScreen.addEventListener('click', () => {
+    splashScreen.style.opacity = 0; 
+    splashScreen.style.transition = "opacity 1s";
+    splashScreen.style.zIndex = -9999;
+    setTimeout(() => {
+      splashScreen.classList.add('splash-hidden')}, 1000);
+  })
+  sessionStorage.setItem("splashTriggered", true)
+}
